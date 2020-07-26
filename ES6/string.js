4dbff1eb29bbccf66a57f0a48df1d39cb8ab492a
janily <07jackychen@gmail.com>
2020-07-26 13:23:41 +0800

    string template

---
+++ ES6/string.js
@@ -0,0 +1,18 @@
+
+console.log(`string text line 1
+string text line 2`);
+
+// 字符串中嵌入变量
+let name = "jacky", time = "today";
+
+console.log(`Hello ${name}, how are you ${time}`)
+
+
+function fn() {
+  return "Hello World";
+}
+
+console.log(`foo ${fn()} bar`)
+
+
+
