a79e18001c663fc064e7a3c01ca5f33108cf7db9
janily <07jackychen@gmail.com>
2020-10-04 22:52:29 +0800

    网页入场动效

--- ES6/obj2arr.js
+++ ES6/obj2arr.js
@@ -1,16 +1,14 @@
-const  reicky = { 
-    name:"reicky chen", 
-    age:18, 
-    born:09-11, 
-    contry:"China" 
+const reicky = {
+    name: "reicky chen",
+    age: 18,
+    born: 09 - 11,
+    contry: "China"
 }
 
 console.log(Object.keys(reicky));
 console.log(Object.values(reicky));
 
-function printUser({name,age,born,country = 'reicky'}) {
-    console.log(`name is ${name}. Age is ${age}`);
-}
 
 
-printUser(reicky);
+
+
