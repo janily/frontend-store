ed530e39f4612c5aaf670f6e54dc8deacef29638
janily <07jackychen@gmail.com>
2020-10-06 23:29:12 +0800

    play button animation

---
+++ effect/play_animation/app.js
@@ -0,0 +1,14 @@
+var pause = document.querySelector('.pause');
+var play = document.querySelector('.play');
+var btn = document.querySelector('#app');
+
+btn.addEventListener('click', () => {
+  if (play.classList.contains("active")) {
+    play.classList.remove("active");
+    pause.classList.add("active");
+  }
+  else {
+    pause.classList.remove("active");
+    play.classList.add("active");
+  }
+})
