30f0cc459ee2448392fefe860698248cb5f7f991
janily <07jackychen@gmail.com>
2020-10-24 18:58:57 +0800

    数组方法

---
+++ ES6/shift.js
@@ -0,0 +1,3 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+array.shift();
+console.log(array);
---
+++ ES6/sign.js
@@ -0,0 +1,6 @@
+const positive = 5;
+const negative = -5;
+const zero = 0;
+console.log(Math.sign(positive));
+console.log(Math.sign(negative));
+console.log(Math.sign(zero)); 
